321fd5bdcef5d190c74ad8f7fe96ee16bd82b956
fredgan06 <406966464@qq.com>
2017-07-29 14:29:42 +0800

    init

---
+++ src/main/java/cn/g_open/xiaoxiaole/XiaoxiaoleApplication.java
@@ -0,0 +1,12 @@
+package cn.g_open.xiaoxiaole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoxiaoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaoxiaoleApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/g_open/xiaoxiaole/XiaoxiaoleApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.g_open.xiaoxiaole;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XiaoxiaoleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
